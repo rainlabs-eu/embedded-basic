273e292be0c4dbd668297de6b84e18884a67d7c3
Krzysztof Wesolowski <krzysztof.wesolowski@volvocars.com>
2020-05-06 17:24:36 +0200

    Add CCITT 0xFFFF variant of CRC

--- rlib-tests/src/crc16_tests.cpp
+++ rlib-tests/src/crc16_tests.cpp
@@ -1,29 +1,53 @@
 #include "rlib/crc16.h"
 
 #include <gmock/gmock.h>
+#include <algorithm>
+#include <boost/crc.hpp>
+#include <climits>
+#include <functional>
+#include <random>
+#include <vector>
 
-struct Crc16TestCase {
-    std::vector<uint8_t> data;
-    uint16_t crc;
+using crc_modbus_type = boost::crc_optimal<16, 0x8005, 0xFFFF, 0, true, true>;
+
+#define CRC_TEST_TYPE(name, function_name, boost_specialization_name)                                    \
+    struct name : public ::testing::Test {                                                               \
+        using result_type = uint16_t;                                                                    \
+        using boost_specialization = boost_specialization_name;                                          \
+        static result_type Calc(const void* data, size_t len) { return rlib::function_name(data, len); } \
+        static result_type Calc(const void* data, size_t len, result_type initial) {                     \
+            return rlib::function_name(data, len, initial);                                              \
+        }                                                                                                \
+    }
+
+CRC_TEST_TYPE(Crc16Modbus, crc16_modbus, crc_modbus_type);
+CRC_TEST_TYPE(Crc16CcittFfff, crc16_ccitt_ffff, boost::crc_ccitt_type);
+
+template <typename CrcXxx>
+struct CrcGenericTest : public ::testing::Test {
+    void CalcCompleteAndCompare(std::vector<uint8_t> array) {
+        auto result = CrcXxx::Calc(array.data(), array.size());
+        typename CrcXxx::boost_specialization bcrc;
+        bcrc.process_bytes(array.data(), array.size());
+        EXPECT_THAT(result, ::testing::Eq(bcrc.checksum()));
+    }
 };
 
-struct Crc16ModbusParamTests : public ::testing::TestWithParam<Crc16TestCase> {};
+using CrcTypes = ::testing::Types<Crc16Modbus, Crc16CcittFfff>;
+TYPED_TEST_SUITE(CrcGenericTest, CrcTypes);
 
-INSTANTIATE_TEST_SUITE_P(Crc16ModbusSampleVectors,
-                         Crc16ModbusParamTests,
-                         ::testing::Values(Crc16TestCase{{0x12, 0x34, 0x56, 0x70}, 0xD67A},
-                                           Crc16TestCase{{0x5A, 0x26, 0x19, 0x77}, 0x81B8}));
+TYPED_TEST(CrcGenericTest, Compiles) {}
 
-TEST_P(Crc16ModbusParamTests, CalculatesOkFromWholeData) {
-    const auto test = GetParam();
-    EXPECT_THAT(rlib::crc16_modbus(test.data.data(), test.data.size()), test.crc);
+TYPED_TEST(CrcGenericTest, CalculatesOkForZeroOrOneBytes) {
+    this->CalcCompleteAndCompare({});
+    this->CalcCompleteAndCompare({0x00});
 }
 
-TEST_P(Crc16ModbusParamTests, CalculatesOkWithFirst3BytesSkipped) {
-    const auto test = GetParam();
-    if (test.data.size() > 3) {
-        auto crc3 = rlib::crc16_modbus(test.data.data(), 3);
-        auto crc_all = rlib::crc16_modbus(test.data.data() + 3, test.data.size() - 3, crc3);
-        EXPECT_THAT(crc_all, test.crc);
-    }
+TYPED_TEST(CrcGenericTest, CalculatesOkForRandom512) {
+    using random_bytes_engine = std::independent_bits_engine<std::default_random_engine, CHAR_BIT, uint8_t>;
+    random_bytes_engine rbe;
+    std::vector<uint8_t> data(512);
+    std::generate(begin(data), end(data), std::ref(rbe));
+
+    this->CalcCompleteAndCompare(data);
 }
--- rlib/include/rlib/crc16.h
+++ rlib/include/rlib/crc16.h
@@ -6,5 +6,6 @@
 namespace rlib {
 
 uint16_t crc16_modbus(const void* data, size_t length, uint16_t last_chunk_crc = 0xFFFF);
+uint16_t crc16_ccitt_ffff(const void* data, size_t length, uint16_t last_chunk_crc = 0xFFFF);
 
 }  // namespace rlib
